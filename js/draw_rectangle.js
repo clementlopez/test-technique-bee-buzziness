af0fc31171d2e9f440a4bfbd3fb085980f4332ab
clement_lopez (DevLopez) <lopezclement31@gmail.com>
2020-09-14 14:04:03 +0200

    Step 1 : create rectangles with random color

--- js/draw_rectangle.js
+++ js/draw_rectangle.js
@@ -11,6 +11,11 @@ class Rectangle {
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+/*
+
+*/
+var color ='#FFFFFF';
+
 /* list_rectangle :
     table listing the drawn rectangles
 */
@@ -22,10 +27,25 @@ var list_rectangle = [];
 var rectangle_in_progress = false;
 
 /*
+    starting_point :
+    starting point of the rectangle that is being drawn
+*/
+var starting_point = {x: 0, y: 0};
+
+/*
+
+*/
+var dimensions_in_progress = { height: 0, width: 0};
+
+/*
     on mousedown event the user starts the creation of a new rectangle
 */
 canvas.addEventListener("mousedown", function (ev){
+    color ='#'+Math.random().toString(16).substr(2,6);
     rectangle_in_progress = true;
+    let mousePosition = getMousePosition(canvas, ev);
+    starting_point.x = mousePosition.mouseX;
+    starting_point.y = mousePosition.mouseY;
 });
 
 /*
@@ -33,13 +53,45 @@ canvas.addEventListener("mousedown", function (ev){
 */
 canvas.addEventListener("mouseup", function (ev){
     rectangle_in_progress = false;
+    let mousePosition = getMousePosition(canvas, ev);
+    if (starting_point.x !== mousePosition.mouseX
+      || starting_point.y !== mousePosition.mouseY) {
+        let rectangle = new Rectangle(
+            starting_point.x,
+            starting_point.y,
+            mousePosition.mouseX - starting_point.x,
+            mousePosition.mouseY - starting_point.y,
+            color);
+        drawRectangle(
+            rectangle.x,
+            rectangle.y,
+            rectangle.height,
+            rectangle.width,
+            rectangle.color
+        );
+        list_rectangle.unshift(rectangle);
+        dimensions_in_progress.height = 0;
+        dimensions_in_progress.width = 0;
+    }
+    console.log(list_rectangle);
 });
 
 /*
     on mousemove event, if the user is drawing a new rectangle the potential final rectangle is drawn following the position of the mouse
 */
 canvas.addEventListener("mousemove", function (ev){
-
+    if(rectangle_in_progress) {
+        let mousePosition = getMousePosition(canvas, ev);
+        drawRectangle(
+            starting_point.x,
+            starting_point.y,
+            mousePosition.mouseX - starting_point.x,
+            mousePosition.mouseY - starting_point.y,
+            color
+        );
+        dimensions_in_progress.height = mousePosition.mouseX - starting_point.x;
+        dimensions_in_progress.width = mousePosition.mouseY - starting_point.y;
+    }
 });
 
 /*
@@ -53,7 +105,11 @@ canvas.addEventListener("dblclick", function (ev){
     drawRectangle will create the final rectangle and add it to list_rectangle
 */
 function drawRectangle(x, y, height, width, color){
-
+    ctx.clearRect(x, y, dimensions_in_progress.height, dimensions_in_progress.width);
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, height, width);
+    ctx.stroke();
 }
 
 /*
@@ -71,7 +127,7 @@ function deleteRectangle(x,y){
 }
 
 function getMousePosition(canvas, ev){
-    var rect = canvas.getBoundingClientRect();
+    let rect = canvas.getBoundingClientRect();
     return {
         mouseX: ev.clientX - rect.left,
         mouseY: ev.clientY - rect.top
