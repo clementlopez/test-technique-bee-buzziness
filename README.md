c5fc515c86962ca370d35572ff4ac7e39d54ed57
clement_lopez (DevLopez) <lopezclement31@gmail.com>
2020-09-15 19:39:40 +0200

    Step 6 : Simulation tests and README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# test-technique-bee-buzziness
+# Test technique BEE-BUZZINESS
+
+## Objectifs
+
+1. Tracer des carrés de couleur aléatoire à la souris
+2. Au double-clic sur un carré lui faire faire une rotation à 360°
+3. A la fin de la rotation supprimer le carré
+4. Si on double-clique sur plusieurs carrés à la fois, attendre la fin de toutes les rotations avant de supprimer les carrés
+
+## Lancer le programme
+
+Rien de plus simple, il suffit d'exécuter la commande suivante
+``npx serve``
+et d'ouvrir votre navigateur préféré à l'adresse
+``http://localhost:5000``
+
+Si vous n'avez pas npm d'installé vous pouver exécuter la commande python
+``python -m http.server 80``
+et d'ouvrir votre navigateur préféré à l'adresse
+``http://localhost/index.html``
+
+Et si vous n'avez pas python vous pouvez ouvrir votre navigateur préféré directement sur le chemin du fichier index.html sur votre machine
+``file:///<chemin-sur-votre-machine>/test-technique-bee-buzziness/index.html``
+
+## Tests
+
+Le résultats des différents tests présents est visible depuis la console de votre navigateur dès l'ouverture de la page
--- test/test.js
+++ test/test.js
@@ -27,7 +27,7 @@
         list_rectangle = [];
     }
     
-    it('should select the good rectangle', function() {
+    it('should selectRectangle select the good rectangle', function() {
         let rectangle1 = new Rectangle(0, 0, 100, 100);
         let rectangle2 = new Rectangle(100, 100, 100, 100);
         list_rectangle.unshift(rectangle1);
@@ -37,7 +37,7 @@
         resetListRectangle();
     });
 
-    it('should select the latest rectangle when multiple good possibilities', function() {
+    it('should selectRectangle select the latest rectangle when multiple good possibilities', function() {
         let rectangle1 = new Rectangle(0, 0, 100, 100);
         let rectangle2 = new Rectangle(10, 10, 100, 100);
         list_rectangle.unshift(rectangle1);
@@ -47,36 +47,84 @@
         resetListRectangle();
     });
 
-    it('should overlap on one axis', function() {
+    it('should overlapOnOneAxis returns true when overlap on one axis', function() {
         assert(overlapOnOneAxis(0, 100, 50, 100) === true);
     });
 
-    it('should not overlap on one axis', function() {
+    it('should overlapOnOneAxis returns false when not overlap on one axis', function() {
         assert(overlapOnOneAxis(0, 10, 10, 10) === false);
     });
 
-    it('should overlap', function() {
+    it('should overlap return true when 2 rectangles overlap', function() {
         let rectangle1 = new Rectangle(25, 25, 50, 50);
         assert(overlap(10, 30, 100, 100, rectangle1) === true);
     });
 
-    it('should not overlap', function() {
+    it('should overlap return false when 2 rectangles not overlap', function() {
         let rectangle1 = new Rectangle(25, 25, 50, 50);
         assert(overlap(0, 0, 25, 25, rectangle1) === false);
     });
 
-    it('should not overlap when only one axis is overlaped', function() {
+    it('should overlap returns false when only one axis is overlaped', function() {
         let rectangle1 = new Rectangle(50, 25, 100, 50);
         assert(overlap(0, 0, 100, 25, rectangle1) === false);
     });
 
-    it('should overlap in rotation', function() {
+    it('should overlapInRotation returns true when a rectangle in rotation overlaps another', function() {
         let rectangle1 = new Rectangle(50, 50, 100, 100);
         assert(overlapInRotation(100, 100, 25, rectangle1) === true);
     });
 
-    it('should not overlap in rotation', function() {
+    it('should overlapInRotation returns false when a rectangle in rotation do not overlaps another', function() {
         let rectangle1 = new Rectangle(50, 50, 25, 25);
         assert(overlapInRotation(100, 100, 25, rectangle1) === false);
+    });
+
+    it('should create a new rectangle', function() {
+        canvas.dispatchEvent(new MouseEvent("mousedown", {clientX: 0, clientY: 0}));
+        canvas.dispatchEvent(new MouseEvent("mouseup", {clientX: 100, clientY: 100}));
+        assert(list_rectangle.length === 1);
+        resetListRectangle();
+        ctx.clearRect(0, 0, 100, 100);
+    });
+
+    /* the simulation of a deletion leading to a visual animation
+        it is preferable to give values out of the visual field for the deletion tests
+    */
+    it('should delete a rectangle', function() {
+        let rectangle = new Rectangle(-500, -500, 100, 100);
+        list_rectangle.unshift(rectangle);
+        canvas.dispatchEvent(new MouseEvent("dblclick", {clientX: -450, clientY: -450}));
+        assert(list_rectangle.length === 0);
+    });
+
+    it('should not delete a rectangle when dblclick is not in the right coordinates', function() {
+        let rectangle = new Rectangle(0, 0, 100, 100);
+        list_rectangle.unshift(rectangle);
+        canvas.dispatchEvent(new MouseEvent("dblclick", {clientX: 200, clientY: 200}));
+        assert(list_rectangle.length === 1);
+        resetListRectangle();
+    });
+
+    it('should delete only the latest rectangle when dblclick on overlapping rectangles', function() {
+        let rectangle1 = new Rectangle(-500, -500, 100, 100);
+        let rectangle2 = new Rectangle(-490, -490, 100, 100);
+        list_rectangle.unshift(rectangle1);
+        list_rectangle.unshift(rectangle2);
+        canvas.dispatchEvent(new MouseEvent("dblclick", {clientX: -450, clientY: -450}));
+        assert(list_rectangle.length === 1);
+        assert(list_rectangle[0].x === -500);
+        resetListRectangle();
+    });
+
+    it('should delete multiple rectangles when multiple dblclicks', function() {
+        let rectangle1 = new Rectangle(-500, -500, 100, 100);
+        let rectangle2 = new Rectangle(-490, -490, 100, 100);
+        list_rectangle.unshift(rectangle1);
+        list_rectangle.unshift(rectangle2);
+        canvas.dispatchEvent(new MouseEvent("dblclick", {clientX: -450, clientY: -450}));
+        canvas.dispatchEvent(new MouseEvent("dblclick", {clientX: -450, clientY: -450}));
+        assert(list_rectangle.length === 0);
+        resetListRectangle();
     });
 })();
